ec138f475541b879bf92b0dba00136c3798c8b43
info <info@centuryinnovations.uk>
2023-10-30 18:16:07 +0000

    logging

--- SpecFlow1/StepDefinitions/LoginSteps.cs
+++ SpecFlow1/StepDefinitions/LoginSteps.cs
@@ -4,13 +4,15 @@ using SpecFlow1.Pages;
 using System;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
+using System.IO;
+using SpecFlow1.Helper;
 
 namespace SpecFlow1.StepDefinitions
 {
     [Binding]
     public class LoginSteps
     {
-
+        private static readonly Log logger = new Log(typeof(LoginSteps));
         private DriverHelper _driverHelper;
         HomePage homePage;
         LoginPage loginPage;
@@ -24,24 +26,28 @@ namespace SpecFlow1.StepDefinitions
         [Given(@"I navigate to application")]
         public void GivenINavigateToApplication()
         {
+            logger.Debug("Navigate to the website.");
             _driverHelper.Driver.Navigate().GoToUrl("https://centuryinnovations.uk/");
         }
 
         [When(@"I click on CONTACT US link")]
         public void WhenIClickOnCONTACTUSLink()
         {
+            logger.Info("Clicking the ContactUs link");
             homePage.ClickContactUs();
         }
 
         [Then(@"I should see Contact Us page")]
         public void ThenIShouldSeeContactUsPage()
         {
+            logger.Debug("Clicking the ContactUs link");
             Assert.That(loginPage.IsContactUsExist(), Is.True, "Contact Us Page is displayed");
         }
 
         [Then(@"I click on CONTACT link")]
         public void ThenIClickOnCONTACTLink()
         {
+            logger.Info("Clicking the Contact link");
             loginPage.ClickContact();
         }
 
@@ -59,6 +65,7 @@ namespace SpecFlow1.StepDefinitions
         [Then(@"I enter details (.*) and (.*)")]
         public void ThenIEnterDetailsNameAndEmail(String name, String email)
         {
+            logger.Info("Enter the name and email");
             loginPage.EnterNameAndEmail(name, email);
            
 
